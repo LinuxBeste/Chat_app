837585be544f64625e30af5d2b76bf11fc265159
linuxbeste <linuxbeste@gmail.com>
2026-07-27 23:00:02 +0200

    Use hermes-v0 transform profile (fixes class+private-field Hermes 0.79 bugs)

--- client/mobile-app/babel.config.js
+++ client/mobile-app/babel.config.js
@@ -1,13 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      ["@babel/plugin-transform-classes"],
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
-      "react-native-reanimated/plugin",
-    ],
+    presets: [["babel-preset-expo", { unstable_transformProfile: "hermes-v0" }]],
+    plugins: ["react-native-reanimated/plugin"],
   }
 }
